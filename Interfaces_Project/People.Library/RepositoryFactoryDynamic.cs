910ef55e4329f701405c93de7fe00b785b3d670d
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-19 20:59:01 +0200

    Dynamic loading of interface (Sample code)

---
+++ Interfaces_Project/People.Library/RepositoryFactoryDynamic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace People.Library
+{
+    //Factory method implemented but this dynamic which means it is compiled at runtime. This is more efficient because with any 
+    //change it does not need recompiling
+    public class RepositoryFactoryDynamic
+    {
+        //Comparing with the factory method this does not take any parameters
+        public static IPersonRepository GetRepository()
+        {
+            //This congig is at app.config of a project- Not implemented here just showing the steps
+            string repoName = ConfigurationManager.AppSettings["RepositoryType"];
+            Type repositoryType = Type.GetType(repoName);
+            object repository = Activator.CreateInstance(repositoryType);
+            IPersonRepository personRepo = repository as IPersonRepository;
+            return personRepo;
+        }
+    }
+}
