1258f9d0d8ee95bda7f0eea563b2a505283966a9
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-19 21:15:52 +0200

    Explicit interface implementation example

---
+++ Interfaces_Project/ExplicitImplementation/OddGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitImplementation
+{
+    //Use of expicit interface implementation
+    public class OddGenerator : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            int i = 1;
+            yield return i;
+            while (true)
+            {
+                i += 2;
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Interfaces_Project/ExplicitImplementation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExplicitImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd numbers : ");
+            var generator = new OddGenerator();
+            foreach (var odd in generator)
+            {
+                if (odd > 50) break;
+                Console.WriteLine(odd);
+            }
+            Console.ReadLine();
+        }
+    }
+}
