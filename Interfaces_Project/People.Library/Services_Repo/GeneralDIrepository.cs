263e23812f806bb4361ff35af0a84f968a605e00
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-19 23:33:29 +0200

    Dependency Injection example

---
+++ Interfaces_Project/People.Library/Services_Repo/GeneralDIrepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace People.Library.Services_Repo
+{
+    //We create this class to show the use of dependency injection
+    //If we ever change the service that we use this class remain the same 
+    public class GeneralDIrepository
+    {
+        private readonly IPersonReader personReader;
+
+        public GeneralDIrepository(IPersonReader personReader)
+        {
+            this.personReader = personReader;
+        }
+        public IEnumerable<Person> GetAll()
+        {
+            return personReader.GetPeople();
+        }
+    }
+}
--- Interfaces_Project/People/Program.cs
+++ Interfaces_Project/People/Program.cs
@@ -1,4 +1,5 @@
 ﻿using People.Library;
+using People.Library.Services_Repo;
 using System;
 using System.Collections.Generic;
 
@@ -10,6 +11,7 @@ namespace People
         {
             //foreach accepts IEnumerable so it is maintainable if something changes
             //So declaring the variable as IEnumerable if we change our data structure in the future, the code here stays the same
+            Console.WriteLine("Without interface example");
             var personRepo = new PersonRepository();
             IEnumerable<Person> persons = personRepo.GetPeople();
             foreach (var person in persons)
@@ -24,6 +26,7 @@ namespace People
 
             //IPersonRepository list_service = new PersonRepository();
             //IPersonRepository list_service = RepositoryFactory.GetRepository("Person");
+            Console.WriteLine("Interface and factory method example");
             IPersonReader list_service = RepositoryFactory.GetRepository("Person");
 
             //var personToAdd = new Person { Id = 3, Name = "Mary" };
@@ -33,9 +36,21 @@ namespace People
             {
                 Console.WriteLine(person.Name);
             }
+            Console.WriteLine("--------------");
 
             //Note : We have to be careful and not use a lot of interfaces because there is the danger that the code will become more complex
             //Go to implentation is weird when using interfaces. Debugging and step into is a solution that take us to the correct implementation
+
+            //Dependency Injection example 
+            Console.WriteLine("Dependency injection example");
+            //If we ever change the service we only modify the following line and we are done 
+            IPersonReader depend_inj_instance = new PersonRepository();
+            var generalRepo = new GeneralDIrepository(depend_inj_instance);
+            IEnumerable<Person> list_service_items2 = generalRepo.GetAll();
+            foreach (var person in list_service_items2)
+            {
+                Console.WriteLine(person.Name);
+            }
         }
     }
 }
